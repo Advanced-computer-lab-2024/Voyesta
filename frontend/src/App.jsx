2fafae9bb4af1ff4e1aed10092311b910f5282d8
zeiad enani <zeiadenani@gmail.com>
2024-09-29 18:02:09 +0300

    Testing branch is working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
+import TestPage from './pages/TestPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        Hello world
-      </div>
+      <TestPage />
     </>
   )
 }
