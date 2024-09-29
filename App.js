b234074c9b586f7a95c1f0a307cb4c36fa18c41b
AbdelrahmanKhairat <155402844+AbdelrahmanKhairat@users.noreply.github.com>
2024-09-29 14:29:40 +0300

    updating the structure of the files

---
+++ App.js
@@ -0,0 +1,33 @@
+// app.js
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Load environment variables
+dotenv.config();
+
+// Create an express application
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+
+// Define routes (for example, a basic home route)
+app.get('/', (req, res) => {
+    res.send('Welcome to the Node.js app connected to MongoDB!');
+});
+
+// Export the app
+module.exports = app;
