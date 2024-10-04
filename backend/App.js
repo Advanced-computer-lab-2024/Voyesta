e527f9c86e0ab05ae54e9a515847ec62436ad558
YoussefAmattar <yousef.matar2003@gmail.com>
2024-10-04 18:02:54 +0200

    Update App.js

--- backend/App.js
+++ backend/App.js
@@ -16,18 +16,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Database connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('MongoDB connection error:', err));
 
-// Define routes (for example, a basic home route)
-app.get('/', (req, res) => {
-    res.send('Welcome to the Node.js app connected to MongoDB!');
-});
+
+
 
 // Export the app
 module.exports = app;
