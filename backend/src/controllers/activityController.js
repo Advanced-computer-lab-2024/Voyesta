173ab3ed2fa3ff852e7e3f5f4bab65ab7aecef1f
ZiadTMahmoud <161654846+ZiadTMahmoud@users.noreply.github.com>
2024-11-06 20:24:43 +0200

    Update activityController.js

--- backend/src/controllers/activityController.js
+++ backend/src/controllers/activityController.js
@@ -52,6 +52,22 @@ const getActivity = async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 };
+const getActivityById = async (req, res) => {
+    try {
+        const activity = await Activity.findById(req.params.id)
+      .populate('category', 'Name')
+      .populate('tags', 'Name')
+      .populate('advertiser', 'username')
+      .populate('ratings.tourist', 'username')
+      .populate('comments.tourist', 'username');
+        if (!activity) {
+            return res.status(404).json({ message: 'Activity not found' });
+        }
+        res.json(activity);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 const getAllActivitiesByAdvertiser = async (req, res) => { 
     console.log(req.user); 
@@ -366,4 +382,4 @@ const checkActivityRatingAndComment = async (req, res) => {
     }
 };
 
-module.exports = { createActivity, getActivity, getAllActivitiesByAdvertiser, updateActivity, addRating, addComment, deleteActivity, sortactivitestsByPrice, sortactivitestsByRatings, filterActivities, filterTouristActivities, search, checkActivityRatingAndComment };
+module.exports = { createActivity,getActivityById, getActivity, getAllActivitiesByAdvertiser, updateActivity, addRating, addComment, deleteActivity, sortactivitestsByPrice, sortactivitestsByRatings, filterActivities, filterTouristActivities, search, checkActivityRatingAndComment };
