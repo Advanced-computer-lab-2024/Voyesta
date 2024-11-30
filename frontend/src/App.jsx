cb2ee17d9baa5c3f0a874a935165211bff893648
ZiadTMahmoud <161654846+ZiadTMahmoud@users.noreply.github.com>
2024-11-30 04:16:49 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -18,7 +18,10 @@ import SellerDashboard from './pages/Dashboards/SellerDashboard'
 import TourismGovernorDashboard from './pages/Dashboards/TourismGovernorDashboard'
 import TouristDashboard from './pages/Dashboards/TouristDashboard';
 import MapView from './newComponents/MapView'
-
+import ActivitiesView from './newComponents/ActivitiesView';
+import ActivityDetail from './newComponents/ActivityDetail';
+import MuseumHistoricalPlaceDetail from './newComponents/MuseumHistoricalPlaceDetail';
+import ItineraryDetail from './newComponents/ItineraryDetail'; // Import the new component
 
 
 
@@ -32,16 +35,23 @@ function App() {
          {/* <SellerDashboard /> */}
 
           <Routes>
-            <Route exact path="/" element={<Signup />}/>
-            <Route exact path="/login" element={<Login />}/>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/admin/*" element={<AdminDashboard />} />
-            <Route path='/tourGuide/*' element={<TourGuideDashboard />} />
-            <Route path='/advertiser/*' element={<AdvertiserDashboard />} />
-            <Route path='/seller/*' element={< SellerDashboard />} />
-            <Route path='/tourismGovernor/*' element={< TourismGovernorDashboard />} />
-            <Route path='/tourist/*' element={< TouristDashboard />} />
-            <Route path='/map' element={<MapView lat="30.0648" lng="31.5102" />} />
+            <Route exact path="/*" element={<Dashboard />} />
+      <Route exact path="/signup" element={<Signup />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/admin/*" element={<AdminDashboard />} />
+      <Route path='/tourGuide/*' element={<TourGuideDashboard />} />
+      <Route path='/advertiser/*' element={<AdvertiserDashboard />} />
+      <Route path='/seller/*' element={< SellerDashboard />} />
+      <Route path='/tourismGovernor/*' element={< TourismGovernorDashboard />} />
+      <Route path='/tourist/*' element={< TouristDashboard />} />
+      <Route path='/guest/*' element={< Dashboard />} />
+      <Route path='/map' element={<MapView lat="30.0648" lng="31.5102" />} />
+      <Route path='/activities' element={<ActivitiesView />} />
+      <Route path='/activity/:id' element={<ActivityDetail />} />
+      <Route path='/museumHistoricalPlace/:id' element={<MuseumHistoricalPlaceDetail />} />
+      <Route path='/itinerary/:id' element={<ItineraryDetail />} /> {/* New route for itinerary detail */}
+
             
           </Routes>
       </div>
